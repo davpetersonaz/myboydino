a039c4da63e87c299550c71f04eedcb660144fda
Dave Peterson <davpeterson@zoho.com>
2020-08-14 18:37:03 -0700

    add junit

--- htmls/templates/header.php
+++ htmls/templates/header.php
@@ -4,9 +4,14 @@
 		<!-- Required meta tags -->
 		<meta charset="utf-8">
 		<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
 		<!-- declare CSS first -->
 		<link href="/css/bootstrap4/bootstrap.css" rel="stylesheet" type="text/css">
 		<link href="/css/style.css?v<?=filemtime($_SERVER['DOCUMENT_ROOT'].'/css/style.css')?>" rel="stylesheet" type="text/css">
+
+		<!-- TODO: Place at the end of the document so the pages load faster -->
+		<script src="/js/jquery/jquery-3.3.1.min.js" type="text/javascript"></script>
+
 		<title>My Boy Dino</title>
 	</head>
 	<body>
